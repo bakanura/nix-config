2da906fd169d2029bf00189ddfde6cbee0e19756
bakasifu <156412177+bakanura@users.noreply.github.com>
2024-05-12 12:29:48 +0200

    asd

--- configuration.nix
+++ configuration.nix
@@ -143,7 +143,6 @@ hardware.bluetooth.settings = {
   boot.kernelModules = [ "iwlwifi" ];
   hardware.enableRedistributableFirmware = true;
   hardware.enableAllFirmware = true;
-  nixpkgs.config.allowUnfree = true;
 
   networking.wireless.enable = true;
   networking.wireless.userControlled.enable = true;
