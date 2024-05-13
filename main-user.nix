15d484531bb453aa79efc97d1b6c45e0cc96f4e1
bakanura <r4twiz@gmail.com>
2024-05-13 13:31:13 +0200

    asasdads

--- main-user.nix
+++ main-user.nix
@@ -1,26 +1,29 @@
 # main-user.nix
 
-{lib, config, pkgs, ...}:
+{ lib, config, pkgs, ... }:
 
+let
+  cfg = config.main-user;
+in
 {
-  options = {
-    main-user.enable
-     = lib.mkEnableOption "eanable user module";
+  options.main-user = {
+    enable 
+      = lib.mkEnableOption "enable user module";
 
-    main-user.userName = lib.mkOption {
+    userName = lib.mkOption {
       default = "bakanura";
       description = ''
-        username
+        baka
       '';
     };
   };
 
-config = lib.mkIf config.main-user.enable {
-  users.users.${config.main-user.userName} = {
-    isNormalUser = true;
-    initialPassword = "12345";
-    description = "baka";
-    shell = pkgs.zsh;
+  config = lib.mkIf cfg.enable {
+    users.users.${cfg.userName} = {
+      isNormalUser = true;
+      initialPassword = "12345";
+      description = "baka";
+      shell = pkgs.zsh;
     };
   };
 }
