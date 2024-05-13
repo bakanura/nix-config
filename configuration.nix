b2c555860159f914a27486d57705bd3401c2d62e
bakasifu <156412177+bakanura@users.noreply.github.com>
2024-05-13 11:35:15 +0200

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -34,7 +34,10 @@ in
       };
     };
   };
-  
+
+  nix.settings = {
+    experimental-features = [ "nix-command" "flakes" ];
+
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
