258911f4b52e15ac46839d4fe14a32097356077f
bakasifu <156412177+bakanura@users.noreply.github.com>
2024-05-13 12:04:13 +0200

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -314,4 +314,4 @@ hardware.bluetooth.settings = {
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "23.11"; # Did you read the comment?
 
-}
+};
