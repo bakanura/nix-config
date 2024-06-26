8b10cf027b416a7e486e4c4a12ca7b37b1f596ee
bakanura <r4twiz@gmail.com>
2024-06-26 10:35:25 +0200

    dadadawdaw

--- configuration.nix
+++ configuration.nix
@@ -1,57 +1,53 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
+{ config, pkgs, ... }:
 
-{ config, pkgs, fetchurl, fetchTarball, ... }:
 let
-  # add unstable channel declaratively
+  # Fetch unstable channel
   unstableTarball =
     builtins.fetchTarball
       "https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz";
 
-  hardwareFramework = builtins.fetchTarball {
-    url = "https://github.com/NixOS/nixos-hardware/archive/master.tar.gz";
-    sha256 = "sha256:174wrxi6670lvx0z2yvq3kd3dvkbwb56fciv6zi5dznyfrxgfdx1";
-  };
-
-  nixpkgs = import (fetchTarball {
-    url = "https://releases.nixos.org/nixos/24.05/nixos-24.05.tar.xz";
-    sha256 = "";
-  }) 
-  {
-    config = {
-      allowUnfree = true;
+  # Fetch hardware framework
+  hardwareFramework =
+    builtins.fetchTarball {
+      url = "https://github.com/NixOS/nixos-hardware/archive/master.tar.gz";
+      sha256 = "sha256:0w6d2nk498i0hqiimfxhxj7i9zhija9sybnhbyknwl7pkc4b7lkp";
     };
-    system = builtins.currentSystem;
-  };
+
+  # Fetch stable NixOS version
+  nixpkgsTarball =
+    builtins.fetchTarball
+      "https://releases.nixos.org/nixos/24.05/nixos-24.05.tar.xz";
 
 in
 {
+  # Import hardware configuration
   imports =
-  [ # include the results of the hardware scan.
-    (import "${hardwareFramework}/framework/13-inch/7040-amd")
-    ./hardware-configuration.nix
-  ];
+    [ # include the results of the hardware scan.
+      (import "${hardwareFramework}/framework/13-inch/7040-amd")
+      ./hardware-configuration.nix
+    ];
 
-  nixpkgs.config = {
-    packageOverrides = pkgs: {
-      unstable = import unstableTarball {
-        config = config.nixpkgs.config;
+  # Override packages in nixpkgs
+  nixpkgs.config.packageOverrides = pkgs: {
+    unstable = import unstableTarball {
+      config = {
+        allowUnfree = true;  # Example of using config settings
       };
     };
   };
-  
-  # Bootloader.
+
+  # Bootloader
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
+  # Networking
   networking.hostName = "nixos"; # Define your hostname.
   networking.networkmanager.enable = true;
+  networking.firewall.enable = false;
+  networking.nameservers = [ "1.1.1.1" "8.8.8.8" ]; # Google's public DNS
 
-  # Set your time zone.
+  # Timezone and internationalization
   time.timeZone = "Europe/Berlin";
-
-  # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "de_DE.UTF-8";
@@ -64,7 +60,8 @@ in
     LC_TELEPHONE = "de_DE.UTF-8";
     LC_TIME = "de_DE.UTF-8";
   };
-  
+
+  # Auto CPU frequency management
   services.auto-cpufreq = {
     enable = true;
     settings = {
@@ -79,32 +76,29 @@ in
     };
   };
 
-  # Enable the X11 windowing system.
+  # X11 and DisplayLink drivers
   services.xserver.enable = true;
-  # Enable DisplayLink Drivers
   services.xserver.videoDrivers = [ "displaylink" "modesetting" ];
 
-  # Enable firmware updates with fwupd
+  # Firmware updates with fwupd
   services.fwupd = {
     enable = true;
     extraRemotes = [ "lvfs-testing" ];
   };
 
-  # Enable the GNOME Desktop Environment.
+  # GNOME Desktop Environment
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
 
-  # Configure keymap in X11
+  # Keyboard and console keymap
   services.xserver.layout = "de";
   services.xserver.xkbVariant = "";
-
-  # Configure console keymap
   console.keyMap = "de";
 
-  # Enable CUPS to print documents.
+  # Printing with CUPS
   services.printing.enable = true;
 
-  # Enable sound with pipewire.
+  # Sound with Pipewire
   sound.enable = true;
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
@@ -118,54 +112,61 @@ in
     };
     pulse.enable = true;
   };
+
+  # Bluetooth settings
   hardware.bluetooth.settings = {
     General = {
       Experimental = true;
     };
   };
 
+  # Shell aliases
   programs.bash.shellAliases = { mixer = "pulsemixer"; };
-  # Enable touchpad support (enabled default in most desktopManager).
+
+  # Libinput for touchpad support
   services.xserver.libinput.enable = true;
 
-  # Enable thermal data
+  # Thermal management
   services.thermald.enable = true;
 
-  # Enable OBS CAM
-  boot.extraModulePackages = with config.boot.kernelPackages; [
-    v4l2loopback
-  ];
-  boot.extraModprobeConfig = ''
-    options v4l2loopback devices=1 video_nr=1 card_label="OBS Cam" exclusive_caps=1
-    options v4l2loopback video_nr=2 card_label="Droidcam" width=640,1920 max_width=1920 height=480,1080 max_height=1080
-  '';
+  # OBS CAM setup
+ # boot.extraModulePackages = with config.boot.kernelPackages; [
+  #  pkgs.v4l2loopback
+  #];
+  #boot.extraModprobeConfig = ''
+  #  options v4l2loopback devices=1 video_nr=1 card_label="OBS Cam" exclusive_caps=1
+  #  options v4l2loopback video_nr=2 card_label="Droidcam" width=640,1920 max_width=1920 height=480,1080 max_height=1080
+  #'';
+
+  # Polkit and RPC
   security.polkit.enable = true;
+  services.rpcbind.enable = true;
 
-  services.rpcbind.enable = true; # needed for NFS
-
+  # NFS setup
   boot.initrd = {
     supportedFilesystems = [ "nfs" ];
     kernelModules = [ "nfs" ];
   };
 
-  fileSystems."/mnt/truenas/nfs" = {
-    device = "192.168.0.251:/mnt/truenas/nfs";
-    fsType = "nfs";
-    options = [ "x-systemd.automount" "noauto" ];
-  };
+#  fileSystems."/mnt/truenas/nfs" = {
+ #   device = "192.168.1.117:/mnt/nextcloud-datapool";
+  #  fsType = "nfs";
+   # options = [ "x-systemd.automount" "noauto" ];
+  #};
 
+  # Fingerprint authentication
   services.fprintd.enable = true;
-
   security.pam.services.swaylock = {
     fprintAuth = true;
   };
 
+  # Logitech wireless
   hardware.logitech.wireless = {
     enable = true;
     enableGraphical = true;
   };
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
+  # User account
   users.users.bakanura = {
     isNormalUser = true;
     description = "bakanura";
@@ -186,9 +187,7 @@ in
       unstable.vscode
       unstable.terraform
       pulseaudioFull
-      # Console mixer
       unstable.pulsemixer
-      # Equalizer on steroids
       unstable.easyeffects
       unstable.ldacbt
       unstable.fprintd
@@ -204,48 +203,43 @@ in
       libreoffice
       ventoy-full
       usbutils
-      mullvad-vpn
-      mullvad
       krita
       nextcloud-client
       ungoogled-chromium
       opentabletdriver
       aws-sso-cli
       oversteer
+      betterdiscordctl
+      linuxKernel.packages.linux_zen.v4l2loopback
+
     ];
   };
 
+  # Mullvad VPN
   services.mullvad-vpn.enable = true;
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-  # Enable and install Steam + prerequisites
+  # Steam and gaming setup
   hardware.steam-hardware.enable = true;
-
   programs.steam = {
     enable = true;
-    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+    remotePlay.openFirewall = true;
+    dedicatedServer.openFirewall = true;
   };
 
-  # Enable automatic login for the user.
+  # Automatic login
   services.xserver.displayManager.autoLogin = {
     enable = true;
     user = "bakanura";
   };
 
-  # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
-  systemd.services."getty@tty1".enable = false;
-  systemd.services."autovt@tty1".enable = false;
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-
+  # System packages
   environment.systemPackages = with pkgs; [
     pkgs.nfs-utils
     pkgs.firefox
-    (pkgs.wrapFirefox (pkgs.firefox-unwrapped.override { pipewireSupport = true;}) {})
+    (pkgs.wrapFirefox (pkgs.firefox-unwrapped.override { pipewireSupport = true; }) {})
     pkgs.android-tools
     pkgs.droidcam
     pkgs.steam
@@ -299,19 +293,14 @@ in
     pkgs.oversteer
     pkgs.aws-sso-cli
     (pkgs.discord.override {
-      # remove any overrides that you don't want
       withOpenASAR = true;
       withVencord = true;
     })
     pkgs.betterdiscordctl
+    pkgs.linuxKernel.packages.linux_zen.v4l2loopback
+
   ];
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "23.11"; # Did you read the comment?
+  # State version
+  system.stateVersion = "23.11";
 }
-
